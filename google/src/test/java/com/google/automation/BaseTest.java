06be5d585394b0538af461fd481b208a2e6f4c0f
Amauri M Mamm <amaurimann@gmail.com>
2025-12-03 19:06:47 -0300

    update
    4 ui tests running

--- google/src/test/java/com/google/automation/BaseTest.java
+++ google/src/test/java/com/google/automation/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Base class for UI tests providing common WebDriver setup and teardown
@@ -18,11 +19,25 @@ public class BaseTest {
      * Setup WebDriver before each test
      */
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        logger.info("WebDriver initialized successfully");
+        try {
+            // Use WebDriverManager to automatically download the correct ChromeDriver
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            // Disable notifications and other popups
+            options.addArguments("--no-first-run");
+            options.addArguments("--no-default-browser-check");
+            options.addArguments("--disable-blink-features=AutomationControlled");
+            options.setExperimentalOption("excludeSwitches", new String[]{"enable-logging"});
+            options.setExperimentalOption("useAutomationExtension", false);
+            
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            logger.info("WebDriver initialized successfully");
+        } catch (Exception e) {
+            logger.error("Failed to initialize WebDriver", e);
+            throw new RuntimeException("WebDriver initialization failed", e);
+        }
     }
 
     /**
